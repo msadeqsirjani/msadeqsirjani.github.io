a979f70ff10363d61a202a8f33ed63fe47b0a4b8
msadeqsirjani <m.sadeq.sirjani@gmail.com>
2025-12-29 01:14:13 -0600

    [refactor]: clean up main.tsx by removing unnecessary comments and ensuring consistent code formatting for improved readability

--- react-portfolio/src/main.tsx
+++ react-portfolio/src/main.tsx
@@ -1,16 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { registerServiceWorker } from './registerSW'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { registerServiceWorker } from './registerSW';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
 
-// Register service worker for PWA support
-registerServiceWorker()
-
-// Note: Analytics is now loaded only after user consent via CookieConsent component
+registerServiceWorker();
