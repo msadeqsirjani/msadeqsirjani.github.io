ca0c932bc3be352bd989e4661ef9027f9617eba0
msadeqsirjani <m.sadeq.sirjani@gmail.com>
2026-07-29 11:31:02 -0700

    style: add editorial typography and justified copy

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import '@fontsource/roboto/latin-400.css';
-import '@fontsource/roboto/latin-500.css';
-import '@fontsource/roboto/latin-700.css';
+import '@fontsource/ibm-plex-sans/latin-400.css';
+import '@fontsource/ibm-plex-sans/latin-500.css';
+import '@fontsource/ibm-plex-sans/latin-600.css';
+import '@fontsource/ibm-plex-sans/latin-700.css';
+import '@fontsource/literata/latin-600.css';
+import '@fontsource/literata/latin-700.css';
 import '@fontsource/cinzel/latin-600.css';
 import './index.css';
 import App from './App.tsx';
